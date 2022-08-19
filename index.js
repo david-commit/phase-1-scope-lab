6fe16bc3538dfac6faadfa41214074522cb75a62
david-commit <david.ondiege@students.moringaschool.com>
2022-08-19 08:46:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+// declare customerName to be bob in global scope & returns the customerName
+var customerName = 'bob';
+
+// modifies the customerName variable to upper string
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+// Declare a variable called bestCustomer in global scope and assigns it to be 'not bob'
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer;
+}
+
+// overwrites the best customer
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+// unsuccessfully tries to reassign the least favorite customer
+function changeLeastFavoriteCustomer(){
+    const bestCustomer = 'its bob';
+    bestCustomer = 'probably bob';
+}
+
+
